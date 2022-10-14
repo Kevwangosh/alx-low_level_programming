936fa5622258a0ad63ae7bb7e44ccfead87dac44
kevwangosh <kevinwangombe@gmail.com>
2022-10-14 10:53:40 -0700

    Task 4-8 C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#iclude <studio.h>
+
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Succesful)
+ */
+int main(void)
+{ 
+	puts(*/"Programming is like building a multilingual puzzle");
+	return (0);
+}
