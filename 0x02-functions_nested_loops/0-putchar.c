7ac859c80cbf467371f05265dc039438b069ed68
kevwangosh <kevinwangombe@gmail.com>
2022-10-25 04:49:30 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,12 @@
 
 int main(void)
 {
-	char text [] = "_putchar";
+	char text[] = "_putchar";
 	int i;
-	for (i = 0; i<8; i++)
+
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(text [i]);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
 	return (0);
