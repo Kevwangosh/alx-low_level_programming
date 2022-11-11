42e4fe4cd93c91b57ed32be93cd941584bf6b027
kevwangosh <kevinwangombe@gmail.com>
2022-11-10 23:30:48 -0800

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isupper - uppercase letters
@@ -9,7 +10,7 @@
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
+		return 1;
 	else
 		return (0);
 }
