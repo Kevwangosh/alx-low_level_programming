d00bc75df8578943c87d46fe4ba4d78f2072598f
kevwangosh <kevinwangombe@gmail.com>
2022-11-11 06:28:31 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: thestring
+ *
+ * Return: the length of string
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+
+	-putchar('\n')
+}
