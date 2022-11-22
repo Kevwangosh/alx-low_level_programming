22951a541a302e7bfcfa71823a2c387eefb4a3c2
kevwangosh <kevinwangombe@gmail.com>
2022-11-21 23:59:45 -0800

    correction

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -42,7 +42,7 @@ void print_all(const char * const format, ...)
 				flag = 1;
 				break;
 		}
-		f (format[i + 1] != '\0' && flag == 0)
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
 		i++;
 	}
